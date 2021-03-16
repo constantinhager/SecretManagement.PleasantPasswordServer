eddc320cef159778e3367540bc5fdc9b1cc57d08
constantinhager <constantin.hager@outlook.de>
2021-03-16 18:03:24 +0100

    Added viewing all passwords at the root level

--- SecretManagement.PleasantPasswordServer.Extension/Public/Get-SecretInfo.ps1
+++ SecretManagement.PleasantPasswordServer.Extension/Public/Get-SecretInfo.ps1
@@ -27,53 +27,148 @@ function Get-SecretInfo
         "Authorization" = "$Token"
     }
 
-    $body = @{
-        "search" = "$Filter"
-    }
-
     $PasswordServerURL = [string]::Concat($AdditionalParameters.ServerURL, ":", $AdditionalParameters.Port)
 
-    $Secrets = Invoke-RestMethod -method post -Uri "$PasswordServerURL/api/v5/rest/search" -body (ConvertTo-Json $body) -Headers $headers -ContentType 'application/json'
-    $id = $Secrets.Credentials.id
+    $Filter = $PSBoundParameters.Filter
 
-    if ($id.Count -gt 1)
+    if ($Filter -ne '*')
     {
-        throw "Multiple ambiguous entries found for $Name, please remove the duplicate entry"
-    }
+        $body = @{
+            "search" = "$Filter"
+        }
 
-    if ($null -eq $id)
+        $Secrets = Invoke-RestMethod -method post -Uri "$PasswordServerURL/api/v5/rest/search" -body (ConvertTo-Json $body) -Headers $headers -ContentType 'application/json'
+        $id = $Secrets.Credentials.id
+
+        if ($id.Count -gt 1)
+        {
+            throw "Multiple ambiguous entries found for $Name, please remove the duplicate entry"
+        }
+
+        if ($null -eq $id)
+        {
+            throw "No secret with $Name is found"
+        }
+
+        $Params = @{
+            Method      = 'GET'
+            Uri         = "$PasswordServerURL/api/v5/rest/Entries/$id"
+            Headers     = $headers
+            ContentType = 'application/json'
+        }
+
+        $CredentialMetadata = Invoke-RestMethod @Params
+
+        $Params = @{
+            Method      = 'GET'
+            Uri         = "$PasswordServerURL/api/v5/rest/folders/$($CredentialMetadata.GroupId)?recurseLevel=0"
+            Headers     = $headers
+            ContentType = 'application/json'
+        }
+        $FolderMetadata = Invoke-RestMethod @Params
+
+        $Name = $CredentialMetadata.Name
+
+        [ReadOnlyDictionary[String, Object]]$metadata = [ordered]@{
+            CustomUserFields = $CredentialMetadata.CustomUserFields
+            Attachments      = $CredentialMetadata.Attachments
+            Tags             = $CredentialMetadata.Tags
+            Url              = $CredentialMetadata.Url
+            Notes            = $CredentialMetadata.Notes
+            Created          = [DateTime]$CredentialMetadata.Created
+            Modified         = [DateTime]$CredentialMetadata.Modified
+            Expires          = [DateTime]$CredentialMetadata.Expires
+            FolderName       = $FolderMetadata.Name
+        } | ConvertTo-ReadOnlyDictionary
+
+        return [SecretInformation]::new(
+            $Name,
+            [SecretType]::PSCredential,
+            $VaultName,
+            $metadata
+        )
+    }
+    else
     {
-        throw "No secret with $Name is found"
+        #TODO: Add Folder recursion for retrieving Credentials in subfolders
+
+        $Params = @{
+            Method      = 'GET'
+            Uri         = "$PasswordServerURL/api/v5/rest/folders/"
+            Headers     = $headers
+            ContentType = 'application/json'
+        }
+
+        $AllFolders = Invoke-RestMethod @Params
+
+        [Object[]]$secretInfoResult = foreach ($Credential in $AllFolders)
+        {
+            # Credentials
+            $AllCredentials = $AllFolders.Credentials
+
+            foreach ($Credential in $AllCredentials)
+            {
+                $Name = $Credential.Name
+
+                $Params = @{
+                    Method      = 'GET'
+                    Uri         = "$PasswordServerURL/api/v5/rest/folders/$($Credential.GroupId)?recurseLevel=0"
+                    Headers     = $headers
+                    ContentType = 'application/json'
+                }
+                $FolderMetadata = Invoke-RestMethod @Params
+
+                [ReadOnlyDictionary[String, Object]]$metadata = [ordered]@{
+                    CustomUserFields = $Credential.CustomUserFields
+                    Attachments      = $Credential.Attachments
+                    Tags             = $Credential.Tags
+                    Url              = $Credential.Url
+                    Notes            = $Credential.Notes
+                    Created          = if ([string]::IsNullOrWhiteSpace($Credential.Created))
+                    {
+                        ''
+                    }
+                    else
+                    {
+                        [DateTime]$Credential.Created
+                    }
+                    Modified         = if ([string]::IsNullOrWhiteSpace($Credential.Modified))
+                    {
+                        ''
+                    }
+                    else
+                    {
+                        [DateTime]$Credential.Modified
+                    }
+                    Expires          = if ([string]::IsNullOrWhiteSpace($Credential.Expires))
+                    {
+                        ''
+                    }
+                    else
+                    {
+                        [DateTime]$Credential.Expires
+                    }
+                    FolderName       = $FolderMetadata.Name
+                } | ConvertTo-ReadOnlyDictionary
+
+                [SecretInformation]::new(
+                    $Name,
+                    [SecretType]::PSCredential,
+                    $VaultName,
+                    $metadata
+                )
+
+            }
+        }
+
+        [Object[]]$sortedInfoResult = $secretInfoResult | Sort-Object -Unique -Property Name
+        if ($sortedInfoResult.count -lt $secretInfoResult.count)
+        {
+            $nonUniqueFilteredRecords = Compare-Object $sortedInfoResult $secretInfoResult -Property Name | Where-Object SideIndicator -eq '=>'
+            Write-Warning "Vault ${VaultName}: Entries with non-unique titles were detected, the duplicates were filtered out.)"
+            Write-Warning "Vault ${VaultName}: Filtered Non-Unique Titles: $($nonUniqueFilteredRecords.Name -join ', ')"
+        }
+
+        return $secretInfoResult
     }
-
-    $Params = @{
-        Method      = 'GET'
-        Uri         = "$PasswordServerURL/api/v5/rest/Entries/$id"
-        Headers     = $headers
-        ContentType = 'application/json'
-    }
-
-    $CredentialMetadata = Invoke-RestMethod @Params
-
-    # TODO: Datemagic
-    # TODO: Only provide Filename and Size
-    # TODO: Return Foldername for GroupID
-    [ReadOnlyDictionary[String, Object]]$metadata = [ordered]@{
-        CustomUserFields = $CredentialMetadata.CustomUserFields
-        Attachments      = $CredentialMetadata.Attachments
-        Tags             = $CredentialMetadata.Tags
-        Url              = $CredentialMetadata.Url
-        Notes            = $CredentialMetadata.Notes
-        Created          = $CredentialMetadata.Created
-        Modified         = $CredentialMetadata.Modified
-        Expires          = $CredentialMetadata.Expires
-
-    } | ConvertTo-ReadOnlyDictionary
-
-    return [SecretInformation]::new(
-        $Filter,
-        [SecretType]::PSCredential,
-        $VaultName,
-        $metadata
-    )
 }
